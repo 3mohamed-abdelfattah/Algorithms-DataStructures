d90fba1d68014c652040301b36ca2100558d630c
MohamedMohamed <m01126165016@icloud.com>
2024-08-12 14:26:34 +0300

    Linked List Implementation

--- Data Structures/LinkedList.js
+++ Data Structures/LinkedList.js
@@ -16,11 +16,47 @@ class LinkedList {
         this.head = new Node(data, this.head);
         this.size++;
     }
+
+    // ADD Last
+    addLast(data) {
+        let node = new Node(data);
+        let current = this.head;
+        if (!this.head) {
+            this.head = node
+        } else {
+            //check if node have next 
+            while (current.next) {
+                current = current.next
+            }
+            //last element (next = null)
+            current.next = node;
+        }
+        this.size++;
+    }
+
+    //Get All Data
+    getAllData() {
+        let current = this.head;
+        while (current) {
+            console.log('current : ', current.data);
+            current = current.next
+        }
+    }
 }
 
-const kk = new LinkedList();
-kk.addFirst(1)
-kk.addFirst(2)
-kk.addFirst(3)
-console.log(kk);
+const linkedList = new LinkedList();
 
+linkedList.addFirst(1)
+linkedList.addFirst(2)
+linkedList.addFirst(3)
+linkedList.addFirst(4)
+linkedList.addFirst(5)
+
+linkedList.addLast(100)
+linkedList.addLast(200)
+linkedList.addLast(300)
+linkedList.addLast(400)
+linkedList.addLast(500)
+
+
+linkedList.getAllData()
