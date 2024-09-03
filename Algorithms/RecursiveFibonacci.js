2ce5140fc627395574ea8afa4ecbeeba971945cf
MohamedMohamed <m01126165016@icloud.com>
2024-09-03 21:11:21 +0300

    Recursive Fibonacci

---
+++ Algorithms/RecursiveFibonacci.js
@@ -0,0 +1,13 @@
+function recursiveFibonacci(n) {
+    if (n < 2) {
+        return n
+    }
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(2));
+console.log(recursiveFibonacci(6));
+
+// Big-O --> O(2^n)
